a019f7fbebb086e9434ebbc2fd58d23e4b83a38d
U-LENOVO-PC1\1 <605123048@qq.com>
2015-08-08 00:03:32 +0800

    add drugdrug_data_continue.py

--- data_prepare/drugdrug_data_continue.py
+++
@@ -1,8 +0,0 @@
-fi = open ("result","r")
-fo = open ("result_final","w")
-line = fi.readlines()
-for i in  range(len(line)):
-	words = line[i].split()
-	fo.write(words[-2]+"\t"+words[-1]+"\n")
-fi.close()
-fo.close()
